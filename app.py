7739d80d4ed3e678e0e0a0f61ffdaccf8c7802c6
Matt Zimmerman <mdz@singly.com>
2012-05-17 15:23:50 -0700

    Dump environment

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def dumpenv():
+    return str(os.environ.items())
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
